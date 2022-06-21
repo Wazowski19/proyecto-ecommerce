f09ee46900f5ee19b22276a6a7536e207b17c22c
Miguel Ángel García Martínez <ma.garmar98@gmail.com>
2022-06-20 20:35:38 -0500

    feat: creation of project structure

---
+++ api/api.js
@@ -0,0 +1,24 @@
+import express from 'express';
+import config from './config/index.js';
+
+const api = express();
+
+api.use(express.json());
+
+api.use((err, req, res, next) => {
+    if (
+      config.server.enviroment === 'development' ||
+      config.server.enviroment === 'local' ||
+      config.server.enviroment === 'test'
+    ) {
+      return res.status(500).json({
+        msg: 'Error',
+        error: err,
+      });
+    }
+    return res.status(500).json({
+      msg: 'Ha ocurrido un error en el server, comunícate con un desarrollador',
+    });
+  });
+  
+  export default api;
