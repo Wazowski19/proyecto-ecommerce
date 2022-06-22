81bee3e0674c08bff32cefe727f1183bca27fa5a
Miguel Ángel García Martínez <ma.garmar98@gmail.com>
2022-06-21 21:35:19 -0500

    feat: first draft for search products

--- api/controllers/productController.js
+++ api/controllers/productController.js
@@ -15,3 +15,26 @@ export const createProduct = async (req, res) =>{
         })
     }
 }
+
+export const searchProduct = async (req, res) =>{
+    const data = req.query;
+
+    try {
+        const product = await productSchema.find(data);
+
+        if(!product ){
+            return res.status(404).json({
+                msg: `Productos no encontrado`
+            });
+        }
+
+        return res.status(200).json({
+            message: "Ok",
+            product,
+        });
+    } catch (error) {
+        return res.status(400).json({
+            message: "Error al obtener los productos solicitados",
+        });
+    }
+}
--- api/routes/productRoutes.js
+++ api/routes/productRoutes.js
@@ -4,5 +4,6 @@ import { productController } from '../controllers/index.js';
 const router = Router();
 
 router.post('/product', productController.createProduct);
+router.get('/findProduct', productController.searchProduct);
 
 export {router}
