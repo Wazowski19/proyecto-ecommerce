f6e3fde56fe2dee3ea17ff225a644e61717e1dc9
Miguel Ángel García Martínez <ma.garmar98@gmail.com>
2022-06-23 20:49:40 -0500

    feat: login creation

--- api/controllers/userController.js
+++ api/controllers/userController.js
@@ -1,9 +1,12 @@
 import { userSchema } from '../models/index.js';
+import bcryp from 'bcrypt';
+import jwt from 'jwt-simple';
 
 export const createUser = async (req, res) =>{
     const {body} = req;
     
     try{
+        body.password = await bcryp.hash(body.password, 3);
         const user = await userSchema.create(body);
         res.status(201).json({
             msg: 'Usuario creado',
@@ -14,4 +17,50 @@ export const createUser = async (req, res) =>{
             msg: 'Error al crear usuario'
         })
     }
+}
+
+export const login = async (req, res) =>{
+    const { email, password } = req.body;
+
+    if (email == undefined || password == undefined) {
+        return res.status(400).json({
+        msg: 'Datos incompletos, ingrese email & password',
+        });
+    }
+    try {
+        const user = await userSchema.findOne({email: `${email}`});
+
+        const match = await bcryp.compare(password, user.password);
+
+        if (!match) {
+        return res.status(401).json({
+            msg: 'Bad credentials',
+        });
+        }
+
+        delete user.password;
+
+        const expDate = new Date();
+        expDate.setMinutes(expDate.getMinutes() + 30);
+
+        const token = jwt.encode(
+        {
+            user,
+            expDate,
+        },
+        process.env.JWT_SECRET
+        );
+        
+        return res.json({
+        msg: 'Usuario encontrado',
+        token,
+        });
+
+        
+    } catch (err) {
+        return res.status(404).json({
+        msg: 'No existe un usuario registrado con ese correo',
+        });
+    }
+
 }
--- api/routes/userRoutes.js
+++ api/routes/userRoutes.js
@@ -4,5 +4,6 @@ import { userController } from '../controllers/index.js';
 const router = Router();
 
 router.post('/user', userController.createUser);
+router.post('/login', userController.login);
 
 export {router}
